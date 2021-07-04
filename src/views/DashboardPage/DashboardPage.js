59950d5c8043629ff4f591db70d61bfbf44c389b
Tetiana Obmetytsia <twinkle.lif.love@gmail.com>
2021-07-04 21:36:23 +0300

    init: dashboardpage

---
+++ src/views/DashboardPage/DashboardPage.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import HomeTab from '../../components/HomeTab';
+
+
+// const DashboardPage = () => {
+//     return (
+//         <div>
+//            <HomeTab />
+//         </div>
+//     );
+// };
+
+// export default DashboardPage;
