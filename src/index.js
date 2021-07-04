cc95bf7937a73a011dee691fcd8682ca437e0acb
Marinessa393 <futurism1303@gmail.com>
2021-07-04 20:12:05 +0300

    fix: commit useless code until we need it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
 import App from './components/App';
 import { ThemeProvider } from '@material-ui/core/styles';
 import variables from './variables';
@@ -11,15 +11,15 @@ import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.store}>
-      <PersistGate loading="wait..." persistor={store.persistor}>
-        <BrowserRouter>
-          <ThemeProvider theme={variables}>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store.store}>
+      <PersistGate persistor={store.persistor}> */}
+    <BrowserRouter>
+      <ThemeProvider theme={variables}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+    {/* </PersistGate>
+    </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,17 +1,17 @@
 import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router';
-import selectors from '../redux/auth/auth-selectors';
+// import selectors from '../redux/auth/auth-selectors';
 
 function PrivateRoute({ component: Component, redirectTo, ...routeProps }) {
-  const isLoggedIn = useSelector(selectors.getIsAuthenticated);
-  return (
-    <Route
-      {...routeProps}
-      render={props =>
-        isLoggedIn ? <Component {...props} /> : <Redirect to={redirectTo} />
-      }
-    />
-  );
+  // const isLoggedIn = useSelector(selectors.getIsAuthenticated);
+  // return (
+  //   <Route
+  //     {...routeProps}
+  //     render={props =>
+  //       isLoggedIn ? <Component {...props} /> : <Redirect to={redirectTo} />
+  //     }
+  //   />
+  // );
 }
 
 export default PrivateRoute;
