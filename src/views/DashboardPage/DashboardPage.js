a7ad1eaa74a64bb1ba7990858e7f4f50d3c97395
Marinessa393 <futurism1303@gmail.com>
2021-07-15 20:23:52 +0300

    init: create separated views

---
+++ src/views/DashboardPage/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeTab from '../../components/HomeTab/HomeTab';
+
+function DashboardPage() {
+  return (
+    <div>
+      <HomeTab />
+    </div>
+  );
+}
+
+export default DashboardPage;
---
+++ src/views/StatsPage/StatsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Stats from '../../components/Stats/Stats';
+
+function StatsPage() {
+  return (
+    <div>
+      <Stats />
+    </div>
+  );
+}
+
+export default StatsPage;
