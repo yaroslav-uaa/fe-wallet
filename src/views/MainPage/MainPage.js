f4398605a47e503f7e153ed6e9b2afdfddc90111
Marinessa393 <futurism1303@gmail.com>
2021-07-16 11:35:19 +0300

    feat: transition group

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -1,15 +1,18 @@
 import React, { lazy } from 'react';
 import { Suspense } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 import Header from '../../components/Header';
 import SideBar from '../../components/SideBar';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import '../ViewsAnimate.css';
 // views
 const DashboardPage = lazy(() => import('../DashboardPage/DashboardPage'));
 const StatsPage = lazy(() => import('../StatsPage/StatsPage'));
 const UserPage = lazy(() => import('../UserPage/UserPage'));
 
 function MainPage() {
+  const location = useLocation();
+
   return (
     <>
       <Header />
@@ -17,9 +20,20 @@ function MainPage() {
         <SideBar />
         <div className="content">
           <Suspense>
-            <Route path="/" exact component={DashboardPage} />
-            <Route path="/stats" exact component={StatsPage} />
-            <Route path="/user" exact component={UserPage} />
+            <TransitionGroup>
+              <CSSTransition
+                timeout={200}
+                classNames="view"
+                key={location.key}
+                unmountOnExit
+              >
+                <Switch location={location}>
+                  <Route path="/" exact component={DashboardPage} />
+                  <Route path="/stats" exact component={StatsPage} />
+                  <Route path="/user" exact component={UserPage} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
           </Suspense>
         </div>
       </div>
