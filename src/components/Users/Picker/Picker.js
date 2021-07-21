4cbff0bb1431a88582bf9d70d95381a98dfcc3a6
Marinessa393 <futurism1303@gmail.com>
2021-07-22 00:35:31 +0300

    refactor picker

--- src/components/Users/Picker/Picker.js
+++ src/components/Users/Picker/Picker.js
@@ -1,29 +1,19 @@
-import { Button, makeStyles } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { CompactPicker } from 'react-color';
 import s from './Picker.module.css';
 const defaultColor = localStorage.getItem('color') || '#212121';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    color: theme.palette.primary.dark,
-    background: 'linear-gradient(60deg, #212121d0, #fffefe90, #212121d0)',
-    marginTop: '35px',
-    fontFamily: 'Poppins',
-    fontWeight: 500,
-  },
-}));
-
 function Picker() {
   const [color, setcolor] = useState('');
-  const stlz = useStyles();
 
   useEffect(() => {
     setcolor(defaultColor);
   }, []);
+
   const handleChangeComplete = color => {
     setcolor(color.hex);
     localStorage.setItem('color', color.hex);
+    document.body.style.backgroundColor = localStorage.color;
   };
 
   return (
@@ -34,18 +24,6 @@ function Picker() {
         onChangeComplete={handleChangeComplete}
         style={{ marginBottom: '49px' }}
       />
-
-      <Button
-        size="medium"
-        className={stlz.button}
-        variant="contained"
-        color="secondary"
-        onClick={() => {
-          window.location.reload();
-        }}
-      >
-        Save Changes
-      </Button>
     </div>
   );
 }
